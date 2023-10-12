bc0f9ddec9ec32458081210489c98c0393ac4a93
Tomek H <tomaszheese@o2.pl>
2023-10-12 21:28:18 +0200

    edit contact book schema

--- contactbook.py
+++ contactbook.py
@@ -1,12 +1,17 @@
+import csv
+
+
 class Contact:
-    def __init__(self, name, address, phone, email, date_of_birth):
+    def __init__(self, name, last_name, address, phone, email, date_of_birth):
         self.name = name   # Ew. ograniczenie ilości znaków
+        self.last_name = last_name  # Ew. ograniczenie ilości znaków
         self.address = address  # Ew. ograniczenie ilości znaków
         self.phone = phone  # Sprawdzanie poprawności wprowadzonego numeru telefonu
         self.email = email  # Sprawdzanie poprawności wprowadzonego email
         self.date_of_birth = date_of_birth  # Sprawdzanie poprawności formatu wprowadzonej daty urodzin
         self.contact = {
             "name": self.name,
+            "last name": self.last_name,
             "address": self.address,
             "phone": self.phone,
             "email": self.email,
@@ -37,21 +42,26 @@ class Contact:
     def date_of_birth(self, new_date_of_birth):
         pass
 
+
 class ContactBook:
-    def __init__(self):
-        pass
-
     def add_contact(self):
-        pass
+        with open("contact_book.csv", "a", newline="") as fh:
+            field_names = ["name", "last name", "address", "phone", "email", "date_of_birth"]
+            writer = csv.DictWriter(fh, fieldnames=field_names)
+            writer.writeheader()
+            writer.writerow() #Instancja klasy Contact w formie słownika
 
-    def change(self):
+    def edit_contact(self):
         pass
 
     def remove_contact(self):
         pass
 
-    def birthday_of_contact(self, days_to_birthday):
+    def search(self):
         pass
 
-    def help(self):
+    def show_all_contacts(self):
+        pass
+
+    def birthday_of_contact(self, days_to_birthday):
         pass

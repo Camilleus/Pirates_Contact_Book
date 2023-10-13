568d5e33e9670650c81f8e0dd952a2b22fa3d256
Krzysztof Jaszewski <KrzysztofJasz2002@gmail.com>
2023-10-13 20:32:40 +0200

    Phone number verification Birth date verification email verification added custom_errors module created WrongInputError

--- contactbook.py
+++ contactbook.py
@@ -1,5 +1,7 @@
 import csv
-
+import re
+from datetime import date
+from custom_errors import WrongInputError
 
 class Contact:
     def __init__(self, name, last_name, address, phone, email, date_of_birth):
@@ -20,27 +22,48 @@ class Contact:
 
     @property
     def phone(self):
-        pass
+        return self._phone
 
     @phone.setter
-    def phone(self, new_phone):
-        pass
+    def phone(self, new_phone:str)->None:
+        pattern=re.compile("\d{9}")
+        if not pattern.fullmatch(new_phone):
+            raise WrongInputError(f"Incorrect phone number: {new_phone}")
+        else:
+            self._phone=new_phone
 
     @property
     def email(self):
-        pass
+        return self._email
 
     @email.setter
-    def email(self, new_email):
-        pass
+    def email(self, new_email:str)->None:   
+        pattern=re.compile("[a-zA-Z][\w.-]+@\w+\.\w{2,}")
+        if not pattern.fullmatch(new_email):
+            raise WrongInputError(f"Incorrect email: {new_email}")
+        else:
+            self._email=new_email
 
     @property
     def date_of_birth(self):
-        pass
+        return self._date_of_birth
 
     @date_of_birth.setter
-    def date_of_birth(self, new_date_of_birth):
-        pass
+    def date_of_birth(self, new_date_of_birth:str)->None: #date expected to be in dd.mm.yyyy format
+        pattern=re.compile("\d{2}\.\d{2}\.\d{4}")
+        if not pattern.fullmatch(new_date_of_birth):
+            raise WrongInputError(f"Incorrect date format: {new_date_of_birth}")
+        ymd_int_date_of_birth=[int(str_number) for str_number in new_date_of_birth.split('.')][::-1]
+        try:
+            self._date_of_birth=date(*ymd_int_date_of_birth)
+        except ValueError as e:
+            if str(e) == 'month must be in 1..12':
+                raise WrongInputError('Incorrect month number')
+            elif str(e) =='day is out of range for month':
+                raise WrongInputError('Wrong day number for this month')
+            else:
+                raise ValueError(e)
+
 
 
 class ContactBook:

1bac9959a5471f297778dac1bb70d5f881577c81
SzHornet <sz.sz2819@gmail.com>
2023-10-16 20:39:18 +0200

    Update contactbook.py

--- contactbook.py
+++ contactbook.py
@@ -1,7 +1,8 @@
 import csv
 import re
-from datetime import date
+from datetime import date, datetime, timedelta
 from custom_errors import WrongInputError
+
 
 class Contact:
     def __init__(self, name, last_name, address, phone, email, date_of_birth):
@@ -10,7 +11,8 @@ class Contact:
         self.address = address  # Ew. ograniczenie ilości znaków
         self.phone = phone  # Sprawdzanie poprawności wprowadzonego numeru telefonu
         self.email = email  # Sprawdzanie poprawności wprowadzonego email
-        self.date_of_birth = date_of_birth  # Sprawdzanie poprawności formatu wprowadzonej daty urodzin
+        # Sprawdzanie poprawności formatu wprowadzonej daty urodzin
+        self.date_of_birth = date_of_birth
         self.contact = {
             "name": self.name,
             "last name": self.last_name,
@@ -18,62 +20,66 @@ class Contact:
             "phone": self.phone,
             "email": self.email,
             "date_of_birth": self.date_of_birth
-            }
+        }
 
     @property
     def phone(self):
         return self._phone
 
     @phone.setter
-    def phone(self, new_phone:str)->None:
-        pattern=re.compile("(?:\+\d{1,3} ?)?(?:\d[- ]?){8}\d")
+    def phone(self, new_phone: str) -> None:
+        pattern = re.compile("(?:\+\d{1,3} ?)?(?:\d[- ]?){8}\d")
         if not pattern.fullmatch(new_phone):
             raise WrongInputError(f"Incorrect phone number: {new_phone}")
         else:
-            normalized_phone_num=''.join(filter(lambda char: char.isdigit(),new_phone))
-            self._phone=normalized_phone_num
+            normalized_phone_num = ''.join(
+                filter(lambda char: char.isdigit(), new_phone))
+            self._phone = normalized_phone_num
 
     @property
     def email(self):
         return self._email
 
     @email.setter
-    def email(self, new_email:str)->None:   
-        pattern=re.compile("[a-zA-Z][\w.-]+@\w+\.\w{2,}")
+    def email(self, new_email: str) -> None:
+        pattern = re.compile("[a-zA-Z][\w.-]+@\w+\.\w{2,}")
         if not pattern.fullmatch(new_email):
             raise WrongInputError(f"Incorrect email: {new_email}")
         else:
-            self._email=new_email
+            self._email = new_email
 
     @property
     def date_of_birth(self):
         return self._date_of_birth
 
     @date_of_birth.setter
-    def date_of_birth(self, new_date_of_birth:str)->None: #date expected to be in dd.mm.yyyy format
-        pattern=re.compile("\d{2}\.\d{2}\.\d{4}")
+    # date expected to be in dd.mm.yyyy format
+    def date_of_birth(self, new_date_of_birth: str) -> None:
+        pattern = re.compile("\d{2}\.\d{2}\.\d{4}")
         if not pattern.fullmatch(new_date_of_birth):
-            raise WrongInputError(f"Incorrect date format: {new_date_of_birth}")
-        ymd_int_date_of_birth=[int(str_number) for str_number in new_date_of_birth.split('.')][::-1]
+            raise WrongInputError(
+                f"Incorrect date format: {new_date_of_birth}")
+        ymd_int_date_of_birth = [
+            int(str_number) for str_number in new_date_of_birth.split('.')][::-1]
         try:
-            self._date_of_birth=date(*ymd_int_date_of_birth)
+            self._date_of_birth = date(*ymd_int_date_of_birth)
         except ValueError as e:
             if str(e) == 'month must be in 1..12':
                 raise WrongInputError('Incorrect month number')
-            elif str(e) =='day is out of range for month':
+            elif str(e) == 'day is out of range for month':
                 raise WrongInputError('Wrong day number for this month')
             else:
                 raise ValueError(e)
 
 
-
 class ContactBook:
     def add_contact(self):
         with open("contact_book.csv", "a", newline="") as fh:
-            field_names = ["name", "last name", "address", "phone", "email", "date_of_birth"]
+            field_names = ["name", "last name", "address",
+                           "phone", "email", "date_of_birth"]
             writer = csv.DictWriter(fh, fieldnames=field_names)
             writer.writeheader()
-            writer.writerow() #Instancja klasy Contact w formie słownika
+            writer.writerow()  # Instancja klasy Contact w formie słownika
 
     def edit_contact(self):
         pass
@@ -85,7 +91,25 @@ class ContactBook:
         pass
 
     def show_all_contacts(self):
-        pass
+        with open('contact_book.csv', newline='') as fh:
+            reader = csv.DictReader(fh)
+            for row in reader:
+                print(row["name"], row['last_name'], row['address'],
+                      row['phone'], row['email'], row['date_of_birth'])
 
-    def birthday_of_contact(self, days_to_birthday):
-        pass
+    def birthdays_in_dyas_range(self, days_range):
+        start_date = datetime.now()
+        end_date = datetime.now() + timedelta(days=days_range)
+        result_list = []
+        with open('contact_book.csv', newline='') as fh:
+            reader = csv.DictReader(fh)
+            for row in reader:
+                date_obj = datetime.strptime(
+                    row["date_of_birth"], '%Y-%m-%d')
+                date_start_year = datetime(year=start_date.year,
+                                           month=date_obj.month, day=date_obj.day)
+                date_end_year = datetime(year=end_date.year,
+                                         month=date_obj.month, day=date_obj.day)
+                if start_date < date_start_year or date_end_year <= end_date:
+                    result_list.append(row)
+        return result_list

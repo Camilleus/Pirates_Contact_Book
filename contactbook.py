bd7c03baba66ce552b8a0822e8fe8907b6c5efa6
SzHornet <sz.sz2819@gmail.com>
2023-10-20 00:26:42 +0200

    metoda birthdays_in_dyas_range update
    Metoda birthdays_in_dyas_range powinna już działać poprawnie, metoda remove contact została dodana, ale nie działa jeszcze tak jak do końca powinna

--- contactbook.py
+++ contactbook.py
@@ -2,6 +2,7 @@ import csv
 import re
 from datetime import date, datetime, timedelta
 from custom_errors import WrongInputError
+import pandas as pd
 
 
 class Contact:
@@ -84,8 +85,12 @@ class ContactBook:
     def edit_contact(self):
         pass
 
-    def remove_contact(self):
-        pass
+    def remove_contact(self, name, last_name):
+        df = pd.read_csv('contact_book.csv')
+        indexDelete = df[(df['name'] == name) & (
+            df['last name'] == last_name)].index
+        df.drop(indexDelete, inplace=True)
+        df.to_csv('contact_book.csv')
 
     def search(self):
         pass
@@ -93,23 +98,50 @@ class ContactBook:
     def show_all_contacts(self):
         with open('contact_book.csv', newline='') as fh:
             reader = csv.DictReader(fh)
+            result_list = []
             for row in reader:
-                print(row["name"], row['last_name'], row['address'],
-                      row['phone'], row['email'], row['date_of_birth'])
+                result_list.append(row)
+            return result_list
 
     def birthdays_in_dyas_range(self, days_range):
         start_date = datetime.now()
         end_date = datetime.now() + timedelta(days=days_range)
-        result_list = []
+        init_list = []
         with open('contact_book.csv', newline='') as fh:
             reader = csv.DictReader(fh)
             for row in reader:
-                date_obj = datetime.strptime(
-                    row["date_of_birth"], '%Y-%m-%d')
-                date_start_year = datetime(year=start_date.year,
-                                           month=date_obj.month, day=date_obj.day)
-                date_end_year = datetime(year=end_date.year,
-                                         month=date_obj.month, day=date_obj.day)
-                if start_date < date_start_year or date_end_year <= end_date:
-                    result_list.append(row)
-        return result_list
+                if row["date_of_birth"]:
+                    date_obj = datetime.strptime(
+                        row["date_of_birth"], '%Y-%m-%d')
+                    date_start_year = datetime(year=start_date.year,
+                                               month=date_obj.month, day=date_obj.day)
+                    date_end_year = datetime(year=end_date.year,
+                                             month=date_obj.month, day=date_obj.day)
+                    if start_date < date_start_year <= end_date or start_date < date_end_year <= end_date:
+                        init_list.append(row)
+                else:
+                    pass
+            if init_list:
+                list_sorted = sorted(
+                    init_list, key=lambda row: row["date_of_birth"][5:])
+                final_list = []
+                for element in list_sorted:
+                    date_obj = datetime.strptime(
+                        element["date_of_birth"], '%Y-%m-%d')
+                    date_to_check = datetime(year=start_date.year,
+                                             month=date_obj.month, day=date_obj.day)
+                    if date_to_check < start_date:
+                        date_to_cal = datetime(
+                            year=start_date.year+1, month=date_obj.month, day=date_obj.day)
+                        delta = date_to_cal - start_date
+                        element['to_birthday'] = str(delta.days)
+                        final_list.append(element)
+                    else:
+                        date_to_cal = datetime(
+                            year=start_date.year, month=date_obj.month, day=date_obj.day)
+                        delta = date_to_cal - start_date
+                        element['to_birthday'] = str(delta.days)
+                        final_list.append(element)
+                return final_list
+            else:
+                return 'No one has a birthday in the given range of days'

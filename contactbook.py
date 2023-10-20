1f7a1eeaac07702a59ca0fe747469752a77e301f
CamilleusRex <camilleusrex@gmail.com>
2023-10-20 19:30:49 +0200

    fixed indentations in  contactbook.py

--- contactbook.py
+++ contactbook.py
@@ -134,64 +134,64 @@ class ContactBook:
                     if is_tag_in_notetags: answer_dict.update({" ".join([contact['name'],contact['last_name']]):contact['note']})
             return answer_dict
 
-  def birthdays_in_days_range(self, days_range):
-          start_date = datetime.now()
-          end_date = datetime.now() + timedelta(days=days_range)
-          init_list = []
-          with open('contact_book.csv', newline='') as fh:
-              reader = csv.DictReader(fh)
-              for row in reader:
-                  if row["_date_of_birth"]:
-                      date_obj = datetime.strptime(
-                          row["_date_of_birth"], '%Y-%m-%d')
+    def birthdays_in_days_range(self, days_range):
+        start_date = datetime.now()
+        end_date = datetime.now() + timedelta(days=days_range)
+        init_list = []
+        with open('contact_book.csv', newline='') as fh:
+            reader = csv.DictReader(fh)
+            for row in reader:
+                if row["_date_of_birth"]:
+                    date_obj = datetime.strptime(
+                        row["_date_of_birth"], '%Y-%m-%d')
 
-                      date_start_year = datetime(year=start_date.year,
+                    date_start_year = datetime(year=start_date.year,
                                                  month=date_obj.month, day=date_obj.day)
-                      date_end_year = datetime(year=end_date.year,
+                    date_end_year = datetime(year=end_date.year,
                                                month=date_obj.month, day=date_obj.day)
 
-                      if start_date < date_start_year <= end_date or start_date < date_end_year <= end_date:
+                    if start_date < date_start_year <= end_date or start_date < date_end_year <= end_date:
                           init_list.append(row)
-                  else:
+                else:
                       pass
-              if init_list:
-                  list_sorted = sorted(
-                      init_list, key=lambda row: row["_date_of_birth"][5:])
-                  final_list = []
-                  for element in list_sorted:
-                      date_obj = datetime.strptime(
-                          element["_date_of_birth"], '%Y-%m-%d')
-                      date_to_check = datetime(year=start_date.year,
+            if init_list:
+                list_sorted = sorted(
+                    init_list, key=lambda row: row["_date_of_birth"][5:])
+                final_list = []
+                for element in list_sorted:
+                    date_obj = datetime.strptime(
+                        element["_date_of_birth"], '%Y-%m-%d')
+                    date_to_check = datetime(year=start_date.year,
                                                month=date_obj.month, day=date_obj.day)
-                      if date_to_check < start_date:
-                          date_to_cal = datetime(
-                              year=start_date.year+1, month=date_obj.month, day=date_obj.day)
-                          delta = date_to_cal - start_date
-                          element['to_birthday'] = str(delta.days)
-                          final_list.append(element)
-                      else:
-                          date_to_cal = datetime(
-                              year=start_date.year, month=date_obj.month, day=date_obj.day)
-                          delta = date_to_cal - start_date
-                          element['to_birthday'] = str(delta.days)
-                          final_list.append(element)
-                  return final_list
-              else:
-                  return 'No one has a birthday in the given range of days'
+                    if date_to_check < start_date:
+                        date_to_cal = datetime(
+                            year=start_date.year+1, month=date_obj.month, day=date_obj.day)
+                        delta = date_to_cal - start_date
+                        element['to_birthday'] = str(delta.days)
+                        final_list.append(element)
+                    else:
+                        date_to_cal = datetime(
+                            year=start_date.year, month=date_obj.month, day=date_obj.day)
+                        delta = date_to_cal - start_date
+                        element['to_birthday'] = str(delta.days)
+                        final_list.append(element)
+                return final_list
+            else:
+                return 'No one has a birthday in the given range of days'
 
-  def search_note_by_tags(self,searched_tags)->dict[str:str]:
-      if isinstance(searched_tags,str):
-        searched_tags=searched_tags.split('#')[1:]
-      answer_dict=dict()
-      with open(self.contact_book_file_path,'r') as fh:
-          list_of_contacts=csv.DictReader(fh,[' ']+self.field_names)
-          for contact in list_of_contacts:
-              is_tag_in_notetags=True
-              if not contact['tags']: continue
-              tags=contact['tags'].split('#')[1:]
-              for tag in searched_tags:
-                  if not contact['note'] or tag not in tags:
-                      is_tag_in_notetags=False
-              if is_tag_in_notetags: answer_dict.update({" ".join([contact['name'],contact['last_name']]):contact['note']})
-      return answer_dict
+    def search_note_by_tags(self,searched_tags)->dict[str:str]:
+        if isinstance(searched_tags,str):
+            searched_tags=searched_tags.split('#')[1:]
+        answer_dict=dict()
+        with open(self.contact_book_file_path,'r') as fh:
+            list_of_contacts=csv.DictReader(fh,[' ']+self.field_names)
+            for contact in list_of_contacts:
+                is_tag_in_notetags=True
+                if not contact['tags']: continue
+                tags=contact['tags'].split('#')[1:]
+                for tag in searched_tags:
+                    if not contact['note'] or tag not in tags:
+                        is_tag_in_notetags=False
+                if is_tag_in_notetags: answer_dict.update({" ".join([contact['name'],contact['last_name']]):contact['note']})
+        return answer_dict
    

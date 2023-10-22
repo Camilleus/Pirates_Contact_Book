a1d2210b1bd81f05daf18990e4b7eb8721e2f1e6
CamilleusRex <camilleusrex@gmail.com>
2023-10-22 11:06:22 +0200

    Update main.py
    removed all commented lines and comments

--- ContactBook/main.py
+++ ContactBook/main.py
@@ -1,11 +1,6 @@
-# from contactbook import Contact, ContactBook
+from contactbook import Contact, ContactBook
 from bot import bot_contact_book
 
 if __name__ == "__main__":
 
     bot_contact_book()
-
-    # kontakt = Contact("Oktawian", "Czakiert", "123-321-123", "mail@wp.pl")
-    # ksiazka = ContactBook()
-    #
-    # ksiazka.add_contact(kontakt)
